eda9790cbbc5238fb648acd05c98cfe8e84c83fb
Leonard Muthoni <leonard.muthoni@equitybank.co.ke>
2022-11-02 01:53:55 +0300

    Finalized LandMark Swift UI App

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,27 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack{
-                Text("Uhuru Gardens")
-                    .font(.subheadline)
-                Spacer()
-                Text("Nairobi")
-                    .font(.subheadline)
-            }
         
+        VStack{
+            MapView()
+                .ignoresSafeArea()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack{
+                    Text("Uhuru Gardens")
+                    Spacer()
+                    Text("Nairobi")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title)
+                Text("Descriptive text goes here ...")
+                
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
-
-
-// Spacer() A spacer expands to make its containing view use all of the space of its parent view, instead of having its size defined only by its contents.
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+// Spacer() A spacer expands to make its containing view use all of the space of its parent view, instead of having its size defined only by its contents.
+
+// alignment: .leading = Edited the VStack initializer to align the views by their leading edges
+
+//.ignoresSafeArea() = To allow the map content to extend to the top edge of the screen, add the ignoresSafeArea(edges: .top) modifier to the map view.
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Leonard Muthoni on 02/11/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+
+
+//You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
+
+//By prefixing a state variable with $, you pass a binding, which is like a reference to the underlying value. When the user interacts with the map, the map updates the region value to match the part of the map that’s currently visible in the user interface.
